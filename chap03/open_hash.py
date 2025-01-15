63997712b58dcc51bf47e5d352f0eb9ca458ad25
ggsong0328 <ggsong0328@gmail.com>
2025-01-15 14:51:02 +0900

    25.01.15

--- chap03/open_hash.py
+++ chap03/open_hash.py
@@ -41,4 +41,48 @@ class OpenHash:
         p = self.table[hash]
 
         for i in range(self.capacity):
-            
+            if p.stat == Status.EMPTY:
+                break
+            elif p.stat == Status.OCCUPIED and p.key == key:
+                return p
+            hash = self.rehash_value(hash)
+            p = self.table[hash]
+        return None
+    
+    def search(self, key: Any) -> Any:
+        p = self.search_node(key)
+        if p is not None:
+            return p.value
+        else:
+            return None
+        
+    def add(self, key: Any, value: Any) -> bool:
+        if self.search(key) is not None:
+            return False
+        
+        hash = self.hash_value(key)
+        p = self.table[hash]
+        for i in range(self.capacity):
+            if p.stat == Status.EMPTY or p.stat == Status.DELETED:
+                self.table[hash] = Bucket(key, value, Status.OCCUPIED)
+                return True
+            hash = self.rehash_value(hash)
+            p = self.table[hash]
+        return False
+    
+    def remove(self, key: Any) -> int:
+        p = self.search_node(key)
+        if p is None:
+            return False
+        p.set_status(Status.DELETED)
+        return True
+    
+    def dump(self) -> None:
+        for i in range(self.capacity):
+            print(f'{i:2} ', end='')
+            if self.table[i].stat == Status.OCCUPIED:
+                print(f'{self.table[i].key}({self.table[i].value})')
+            elif self.table[i].stat == Status.EMPTY:
+                print('-- 미동록 --')
+            elif self.table[i].stat == Status.DELETED:
+                print('-- 삭제 완료 --')
